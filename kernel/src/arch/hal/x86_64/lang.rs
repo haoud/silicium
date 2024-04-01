7a2815a335dedff900bd461d299b88adba04005d
Haoud <cadilhac.romain@gmail.com>
2024-04-01 14:11:00 +0200

    Improved panic handler when neested panics occur

--- kernel/src/arch/hal/x86_64/lang.rs
+++ kernel/src/arch/hal/x86_64/lang.rs
@@ -1,11 +1,25 @@
-use crate::arch::x86_64::{apic, cpu, irq, smp};
+use crate::arch::x86_64::{
+    apic,
+    apic::local::{IpiDestination, IpiPriority},
+    cpu, smp,
+};
+use core::sync::atomic::AtomicBool;
 
-#[cfg(feature = "panic_info")]
+/// This flag is used to prevent the kernel from panicking multiple times in a row. This often
+/// means that the kernel has panicked in the panic handler itself, indicating a critical error
+/// during the panic handling process.
+static PANICKED: AtomicBool = AtomicBool::new(false);
+
+#[atomic]
 #[panic_handler]
+#[cfg(feature = "panic_info")]
 pub fn panic(info: &core::panic::PanicInfo) -> ! {
-    use crate::arch::x86_64::apic::local::{IpiDestination, IpiPriority};
+    // If we have already panicked, we should not panic again because it means that
+    // we have panicked in the panic handler itself.
+    if PANICKED.swap(true, core::sync::atomic::Ordering::SeqCst) {
+        cpu::halt();
+    }
 
-    irq::disable();
     if smp::ap_booted() {
         // SAFETY: This is safe because we have ensured that the APs are booted, meaning
         // that they can safely receive IPIs without triple faulting.
@@ -29,12 +43,10 @@ pub fn panic(info: &core::panic::PanicInfo) -> ! {
     cpu::halt();
 }
 
-#[cfg(not(feature = "panic_info"))]
+#[atomic]
 #[panic_handler]
+#[cfg(not(feature = "panic_info"))]
 pub fn panic(_: &core::panic::PanicInfo) -> ! {
-    use crate::arch::x86_64::apic::local::{IpiDestination, IpiPriority};
-
-    irq::disable();
     if smp::ap_booted() {
         // SAFETY: This is safe because we have ensured that the APs are booted, meaning
         // that they can safely receive IPIs without triple faulting.
