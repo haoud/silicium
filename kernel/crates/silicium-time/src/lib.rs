f9e54e8e9959b026c528a068302fe1ec956f91fb
Haoud <cadilhac.romain@gmail.com>
2024-04-07 10:57:06 +0200

    Improved timespec

--- kernel/crates/silicium-time/src/lib.rs
+++ kernel/crates/silicium-time/src/lib.rs
@@ -5,40 +5,10 @@ use core::{
     fmt::Display,
     ops::{Add, AddAssign, Sub, SubAssign},
 };
-use unit::{Nanosecond, Nanosecond32, Second, Second32};
+use unit::{Nanosecond, Second};
 
 pub mod frequency;
 pub mod unit;
-
-/// A timestamp that represents a point in time with a 32-bit resolution.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Timespec32 {
-    pub seconds: Second32,
-    pub nano: Nanosecond32,
-}
-
-impl Timespec32 {
-    /// Create a new 32-bits timestamp with the given seconds and nanoseconds. It does
-    /// not modify the values to have a "conformant" timestamp (i.e. the nanoseconds
-    /// are less than `1_000_000_000`).
-    #[must_use]
-    pub const fn new(seconds: Second32, nano: Nanosecond32) -> Self {
-        Self { seconds, nano }
-    }
-
-    /// Create a new conformant 32-bits timestamp with the given seconds and
-    /// nanoseconds. It nanoseconds are greater than `1_000_000_000`, it will
-    /// increment the seconds accordingly and adjust the nanoseconds.
-    #[must_use]
-    pub fn conform(second: Second32, nano: Nanosecond32) -> Self {
-        let mut seconds = second;
-        let mut nano = nano;
-
-        seconds.0 += nano.0 / 1_000_000_000;
-        nano.0 %= 1_000_000_000;
-        Self { seconds, nano }
-    }
-}
 
 /// A timestamp that represents a point in time.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -56,6 +26,15 @@ impl Timespec {
         Self { seconds, nano }
     }
 
+    /// Create a new timestamp with all fields set to zero.
+    #[must_use]
+    pub const fn zero() -> Self {
+        Self {
+            seconds: Second(0),
+            nano: Nanosecond(0),
+        }
+    }
+
     /// Create a new conformant timestamp with the given seconds and
     /// nanoseconds. It nanoseconds are greater than `1_000_000_000`, it will
     /// increment the seconds accordingly and adjust the nanoseconds.
@@ -67,6 +46,11 @@ impl Timespec {
         seconds.0 += nano.0 / 1_000_000_000;
         nano.0 %= 1_000_000_000;
         Self { seconds, nano }
+    }
+
+    fn normalize(&mut self) {
+        self.seconds.0 += self.nano.0 / 1_000_000_000;
+        self.nano.0 %= 1_000_000_000;
     }
 }
 
@@ -89,15 +73,35 @@ impl Add<Nanosecond> for Timespec {
     }
 }
 
+impl Add<Timespec> for Timespec {
+    type Output = Self;
+
+    fn add(self, rhs: Timespec) -> Self::Output {
+        let mut copy = self;
+        copy += rhs.seconds;
+        copy += rhs.nano;
+        self
+    }
+}
+
 impl AddAssign<Second> for Timespec {
     fn add_assign(&mut self, rhs: Second) {
-        *self = *self + rhs;
+        self.seconds += rhs;
     }
 }
 
 impl AddAssign<Nanosecond> for Timespec {
     fn add_assign(&mut self, rhs: Nanosecond) {
-        *self = *self + rhs;
+        self.nano += rhs;
+        self.normalize();
+    }
+}
+
+impl AddAssign<Timespec> for Timespec {
+    fn add_assign(&mut self, rhs: Timespec) {
+        self.seconds += rhs.seconds;
+        self.nano += rhs.nano;
+        self.normalize();
     }
 }
 
@@ -115,14 +119,29 @@ impl Sub<Second> for Timespec {
 impl Sub<Nanosecond> for Timespec {
     type Output = Self;
 
-    fn sub(self, mut rhs: Nanosecond) -> Self::Output {
-        let mut seconds = self.seconds;
-        while rhs >= Nanosecond::new(1_000_000_000) {
-            rhs += Nanosecond::new(1_000_000_000);
-            seconds -= Second::new(1);
-        }
+    fn sub(self, rhs: Nanosecond) -> Self::Output {
+        if rhs.0 > self.nano.0 {
+            let mut seconds = self.seconds;
+            let mut nano = self.nano;
 
-        Self::conform(seconds, rhs)
+            let diff = rhs.0 - nano.0;
+            nano.0 = 1_000_000_000 - diff;
+            seconds -= Second(rhs.0 / 1_000_000_000 + 1);
+            Self::conform(seconds, nano)
+        } else {
+            Self::conform(self.seconds, self.nano - rhs)
+        }
+    }
+}
+
+impl Sub<Timespec> for Timespec {
+    type Output = Self;
+
+    fn sub(self, rhs: Timespec) -> Self::Output {
+        let mut copy = self;
+        copy -= rhs.seconds;
+        copy -= rhs.nano;
+        copy
     }
 }
 
@@ -138,8 +157,14 @@ impl SubAssign<Nanosecond> for Timespec {
     }
 }
 
+impl SubAssign<Timespec> for Timespec {
+    fn sub_assign(&mut self, rhs: Timespec) {
+        *self = *self - rhs;
+    }
+}
+
 impl Display for Timespec {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{}.{} s", self.seconds.0, self.nano.0)
+        write!(f, "{}.{:09} s", self.seconds.0, self.nano.0)
     }
 }
